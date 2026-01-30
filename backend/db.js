b98ff57e2039f29a5f5037f8a147907012039372
jihadyaghi <jihadyaghie@gmail.com>
2026-01-31 01:19:58 +0200

    connect backend to mysql

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql2");
+const db = mysql.createPool({
+    host:process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
+});
+module.exports = db;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
+const db = require("./db");
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("MarketHouse API is running 🚀");
+app.get("/" , (req,res)=>{
+    res.send("MarketHouse API is runing");
 });
-
+app.get("/test-db" , async (req,res)=>{
+    try{
+        const [rows] = await db.promise().query("Show tables");
+        res.json(rows);
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({error:"DB error" , details:err.message});
+    }
+});
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT , ()=> console.log("Server runing on port " + PORT))
