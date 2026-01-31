96de63e2147d04aab318e006f40eb1d75b5364e1
jihadyaghi <jihadyaghie@gmail.com>
2026-01-31 02:48:15 +0200

    Edit server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const app = require("./app");
-const PORT = process.env.MYSQLPORT || 5000;
-app.listen(PORT , ()=>{
-    console.log("Server running on port " + PORT);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
